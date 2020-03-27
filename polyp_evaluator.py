1ff5c9880ed0a7d83a659ece029eb7dda7943ac5
Anand Kurapati <akurapa2@asu.edu>
2020-03-27 09:26:43 -0500

    Adding orb detector

--- polyp_evaluator.py
+++ polyp_evaluator.py
@@ -14,6 +14,10 @@ from sklearn import svm
 
 from CNNIQAnet import CNNIQAnet
 from IQADataset import NonOverlappingCropPatches
+from imglibs.orb import orb
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+
 
 if __name__ == "__main__":
 
@@ -44,6 +48,7 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         clear_scores = []
+        clear_orb_scores=[]
 
         for image in clear_images:
             path = clear_dir + image
@@ -53,10 +58,12 @@ if __name__ == "__main__":
             score = patch_scores.mean().item()
             print(image + ":" + str(score))
             clear_scores.append(score)
+            clear_orb_scores.append(orb(path))
 
         print("################")
 
         blur_scores = []
+        blur_orb_scores = []
         for image in blur_images:
             path = blur_dir + image
             im = Image.open(path).convert('L')
@@ -65,26 +72,32 @@ if __name__ == "__main__":
             score = patch_scores.mean().item()
             print(image + ":" + str(score))
             blur_scores.append(score)
+            blur_orb_scores.append(orb(path))
 
         SVM_TRAIN_RATIO = 0.6
 
         X, y = [], []
 
         for i in range(int(len(clear_scores) * SVM_TRAIN_RATIO)):
-            X.append([clear_scores[i],0])
+            X.append([clear_scores[i],clear_orb_scores[i]])
             y.append(0)
 
         for i in range(int(len(blur_scores) * SVM_TRAIN_RATIO)):
-            X.append([blur_scores[i],0])
+            X.append([blur_scores[i],blur_orb_scores[i]])
             y.append(1)
 
         clf = svm.SVC(kernel='linear', C=1.0)
         clf.fit(X, y)
 
+        Y_true=[]
+        Y_pred=[]
+
         misPredicts = 0
         for i in range(int(len(clear_scores) * SVM_TRAIN_RATIO), len(clear_scores)):
-            x = [[clear_scores[i],0]]
+            x = [[clear_scores[i],clear_orb_scores[i]]]
             y = clf.predict(x)
+            Y_true.append(0)
+            Y_pred.append(y)
             true_value = "Clear "
             pred_value = "Clear"
             if y[0] == 1:
@@ -94,8 +107,10 @@ if __name__ == "__main__":
             print("Clear | score :" + str(x) + pred_value)
 
         for i in range(int(len(blur_scores) * SVM_TRAIN_RATIO), len(blur_scores)):
-            x = [[blur_scores[i],0]]
+            x = [[blur_scores[i],blur_orb_scores[i]]]
             y = clf.predict(x)
+            Y_true.append(1)
+            Y_pred.append(y)
             true_value = "Blurry "
             pred_value = "Blurry"
             if y[0] == 0:
@@ -109,3 +124,8 @@ if __name__ == "__main__":
 
         print("Total :" + str(total) + " Mispredicted :" +
               str(misPredicts))
+
+        print("Accuracy :"+str(accuracy_score(Y_true, Y_pred)))
+        roc_auc_score(Y_true, Y_pred)
+
+
